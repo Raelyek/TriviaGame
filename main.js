566dc88a74fb75587fd2e8120a8e3f3235fe1369
Vidal <0326362@students.dadeschools.net>
2019-04-16 14:19:44 -0400

    Trivia

---
+++ main.js
@@ -0,0 +1,15 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30
+
+// Question Set
+var questions = [
+
+    {
+        question: "Which Daedric Prince created Malacath?",
+        answers: ["Boethiah," "Hermaeus Mora", "Meridia", "Mehrunes Dagon"],
+        correctAnswer: "Boethiah",
+        image: ""
+    }
+
+]
